8b2d6ae34f0477060bc95c7892712285ab672b7f
gojosatorukz <242291@astanait.edu.kz>
2025-01-08 05:54:54 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         TransportService transportService = new TransportService();
 
-        // Создаем автобусы
         Bus bus1 = new Bus("B001", "Route A", 50);
         Bus bus2 = new Bus("B002", "Route B", 40);
         Bus bus3 = new Bus("B003", "Route C", 30);
@@ -11,22 +10,18 @@ public class Main {
         transportService.addBus(bus2);
         transportService.addBus(bus3);
 
-        // Создаем пассажиров
         Commuter commuter1 = new Commuter("Alice", "T001");
         Commuter commuter2 = new Commuter("Bob", "T002");
 
         transportService.addCommuter(commuter1);
         transportService.addCommuter(commuter2);
 
-        // Вывод всех автобусов
         System.out.println("All Buses:");
         transportService.printAllBuses();
 
-        // Фильтрация автобусов по вместимости
         System.out.println("\nBuses with capacity >= 40:");
         transportService.getBusesByCapacity(40).forEach(System.out::println);
 
-        // Сортировка автобусов по вместимости
         System.out.println("\nBuses sorted by capacity:");
         transportService.sortBusesByCapacity().forEach(System.out::println);
     }
